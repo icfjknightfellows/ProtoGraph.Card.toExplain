831283eb94f8e69ea1b76848eff328c68e5380ff
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-06-17 12:22:12 +0530

    removed commented code

--- Container.jsx
+++ Container.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-// import Truncate from 'react-truncate';
-// import ExplainerCard from './cards/explainer_card.jsx'
 
 export default class ExplainerCard extends React.Component {
   constructor(props) {
@@ -16,13 +14,8 @@ export default class ExplainerCard extends React.Component {
       }
     } 
     this.state = {
-      card_data: [],
-      // expanded: false,
-      // truncated: false,
-      // card_styles: card_styles
+      card_data: []
     }
-    // this.handleTruncate = this.handleTruncate.bind(this);
-    // this.toggleLines = this.toggleLines.bind(this);
   }
 
   componentDidMount() {
@@ -40,48 +33,6 @@ export default class ExplainerCard extends React.Component {
     }   
   }
 
-  // handleTruncate(truncated) {
-  //   // console.log(truncated);
-  //   if (this.state.truncated !== truncated) {
-  //     this.setState({
-  //       truncated
-  //     });
-  //   }
-  // }
-
-  // toggleLines(event) {
-  //   event.preventDefault();
-  //   let card_offset = document.getElementById('main-card-div').offsetHeight,
-  //     text_offset = document.getElementById('explainer-text').offsetHeight;
-  //     console.log(card_offset, "card_offset")
-  //     console.log(text_offset, "text_offset")
-  //   // console.log("in toogle event --- expanded", this.state.expanded)
-  //   // console.log("in toogle event --- truncated",this.state.truncated)
-  //   if (!this.state.expanded) {
-  //     // console.log("inside if --- before - expanded", this.state.expanded)
-  //     // console.log("inside if  --- before - truncated",this.state.truncated)
-  //     this.setState({
-  //       expanded: !this.state.expanded,
-  //       card_styles: {
-  //         height: card_offset + text_offset + 18
-  //       }
-  //     });
-  //     // console.log("inside if --- after - expanded", this.state.expanded)
-  //     // console.log("inside if  --- after - truncated",this.state.truncated)
-  //   } else {
-  //     // console.log("inside else --- before - expanded", this.state.expanded)
-  //     // console.log("inside else --- before - truncated",this.state.truncated)
-  //     this.setState({
-  //       expanded: !this.state.expanded,
-  //       card_styles: {
-  //         height: text_offset + 18
-  //       }
-  //     })
-  //     // console.log("inside else --- after - expanded", this.state.expanded)
-  //     // console.log("inside else --- after - truncated",this.state.truncated)
-  //   }
-  // }
-
   getScreenSize() {
     let w = window,
       d = document,
@@ -97,28 +48,6 @@ export default class ExplainerCard extends React.Component {
   }
 
   render() {
-    // const { more, less, lines } = this.props;
-    // const { expanded, truncated } = this.state;
-  
-    // return (
-    //   <div className='cards-container'> 
-    //     <div id="main-card-div" className = "card-div" style = {this.state.card_styles}>
-    //       <h1 className="explainer_header">{this.state.card_data.explainer_header}</h1>
-    //       <div id="explainer-text" className="explainer_text" style= {this.state.explainer_text_styles}>
-    //         <Truncate 
-    //           lines={!expanded && lines}
-    //           ellipsis={(
-    //             <span>... <a id="read-more-link" href='#' onClick={this.toggleLines}>{more}</a></span>
-    //           )}>
-    //            {this.state.card_data.explainer_text}
-    //         </Truncate>
-    //       </div>
-    //       {!truncated && expanded && (
-    //         <span> <a id="read-more-link" href='#' onClick={this.toggleLines}>{less}</a></span>
-    //       )}
-    //     </div>
-    //   </div>
-    // );
     return (
       <div id="main-card-div" className = "card-div" style = {this.state.card_styles}>
         <h1 className="explainer_header"> {this.state.card_data.explainer_header} </h1>
@@ -129,11 +58,3 @@ export default class ExplainerCard extends React.Component {
     )
   }
 }
-
-// ExplainerCard.defaultProps = {
-//   lines: 4,
-//   more: 'Read more',
-//   less: 'Show less'
-// };
-
-// export default Container;
--- dist/v0.0.1/cards.js
+++ dist/v0.0.1/cards.js
@@ -21963,9 +21963,6 @@ var Pro =
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	// import Truncate from 'react-truncate';
-	// import ExplainerCard from './cards/explainer_card.jsx'
-
 	var ExplainerCard = function (_React$Component) {
 	  _inherits(ExplainerCard, _React$Component);
 
@@ -21985,13 +21982,8 @@ var Pro =
 	    }
 	    _this.state = {
 	      card_data: []
-	      // expanded: false,
-	      // truncated: false,
-	      // card_styles: card_styles
-
-	      // this.handleTruncate = this.handleTruncate.bind(this);
-	      // this.toggleLines = this.toggleLines.bind(this);
-	    };return _this;
+	    };
+	    return _this;
 	  }
 
 	  _createClass(ExplainerCard, [{
@@ -22011,49 +22003,6 @@ var Pro =
 	        });
 	      }
 	    }
-
-	    // handleTruncate(truncated) {
-	    //   // console.log(truncated);
-	    //   if (this.state.truncated !== truncated) {
-	    //     this.setState({
-	    //       truncated
-	    //     });
-	    //   }
-	    // }
-
-	    // toggleLines(event) {
-	    //   event.preventDefault();
-	    //   let card_offset = document.getElementById('main-card-div').offsetHeight,
-	    //     text_offset = document.getElementById('explainer-text').offsetHeight;
-	    //     console.log(card_offset, "card_offset")
-	    //     console.log(text_offset, "text_offset")
-	    //   // console.log("in toogle event --- expanded", this.state.expanded)
-	    //   // console.log("in toogle event --- truncated",this.state.truncated)
-	    //   if (!this.state.expanded) {
-	    //     // console.log("inside if --- before - expanded", this.state.expanded)
-	    //     // console.log("inside if  --- before - truncated",this.state.truncated)
-	    //     this.setState({
-	    //       expanded: !this.state.expanded,
-	    //       card_styles: {
-	    //         height: card_offset + text_offset + 18
-	    //       }
-	    //     });
-	    //     // console.log("inside if --- after - expanded", this.state.expanded)
-	    //     // console.log("inside if  --- after - truncated",this.state.truncated)
-	    //   } else {
-	    //     // console.log("inside else --- before - expanded", this.state.expanded)
-	    //     // console.log("inside else --- before - truncated",this.state.truncated)
-	    //     this.setState({
-	    //       expanded: !this.state.expanded,
-	    //       card_styles: {
-	    //         height: text_offset + 18
-	    //       }
-	    //     })
-	    //     // console.log("inside else --- after - expanded", this.state.expanded)
-	    //     // console.log("inside else --- after - truncated",this.state.truncated)
-	    //   }
-	    // }
-
 	  }, {
 	    key: 'getScreenSize',
 	    value: function getScreenSize() {
@@ -22072,28 +22021,6 @@ var Pro =
 	  }, {
 	    key: 'render',
 	    value: function render() {
-	      // const { more, less, lines } = this.props;
-	      // const { expanded, truncated } = this.state;
-
-	      // return (
-	      //   <div className='cards-container'> 
-	      //     <div id="main-card-div" className = "card-div" style = {this.state.card_styles}>
-	      //       <h1 className="explainer_header">{this.state.card_data.explainer_header}</h1>
-	      //       <div id="explainer-text" className="explainer_text" style= {this.state.explainer_text_styles}>
-	      //         <Truncate 
-	      //           lines={!expanded && lines}
-	      //           ellipsis={(
-	      //             <span>... <a id="read-more-link" href='#' onClick={this.toggleLines}>{more}</a></span>
-	      //           )}>
-	      //            {this.state.card_data.explainer_text}
-	      //         </Truncate>
-	      //       </div>
-	      //       {!truncated && expanded && (
-	      //         <span> <a id="read-more-link" href='#' onClick={this.toggleLines}>{less}</a></span>
-	      //       )}
-	      //     </div>
-	      //   </div>
-	      // );
 	      return _react2.default.createElement(
 	        'div',
 	        { id: 'main-card-div', className: 'card-div', style: this.state.card_styles },
@@ -22120,15 +22047,6 @@ var Pro =
 
 	  return ExplainerCard;
 	}(_react2.default.Component);
-
-	// ExplainerCard.defaultProps = {
-	//   lines: 4,
-	//   more: 'Read more',
-	//   less: 'Show less'
-	// };
-
-	// export default Container;
-
 
 	exports.default = ExplainerCard;
 
