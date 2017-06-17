880579330c7acebb1974226142605ea31092910b
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-06-17 12:31:39 +0530

    changed file names, added assets and minified the code

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,47 @@
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './main.js',
-    output: {
-        library: 'Pro',
-        path: './',
-        filename: './dist/v0.0.1/cards.js',
-    },
-    // plugins: [
-       // new webpack.optimize.UglifyJsPlugin()
-    // ],
-    // plugins: [
-    //     new webpack.DefinePlugin({
-    //       'process.env': {
-    //         NODE_ENV: JSON.stringify('production')
-    //       }
-    //     }),
-    //     // new webpack.IgnorePlugin(/cards/),
-    //     new webpack.optimize.UglifyJsPlugin()
-    // ],
-    node: {
-        net: 'empty',
-        tls: 'empty',
-        fs: 'empty'
-    },
-    // externals: {
-    //     'cards': 'cards'
-    // },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query:
-                {
-                    presets:['react']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loaders: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }
-        ]
-    }
+  entry: './main.js',
+  output: {
+    library: 'Pro',
+    path: './',
+    filename: './dist/v0.0.1/card.min.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    fs: 'empty'
+  },
+  // externals: {
+  //     'cards': 'cards'
+  // },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:
+        {
+          presets:['react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
 };
