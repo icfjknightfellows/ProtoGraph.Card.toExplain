d28d1bd961856a09521c761b86a7a8b6447f0077
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-06-18 20:11:10 +0530

    seperated json file for data and configuration

--- main.js
+++ main.js
@@ -34,6 +34,19 @@ ProtoGraph.Card.toExplain.prototype.renderLaptop = function (data) {
     this.options.selector);
 }
 
+ProtoGraph.Card.toExplain.prototype.renderMobile = function (data) {
+  this.mode = 'mobile';
+  ReactDOM.render(
+    <ExpCard
+      dataURL={this.options.data_url}
+      schemaURL={this.options.schema_url}
+      mode={this.mode}
+      ref={(e) => {
+        this.containerInstance = this.containerInstance || e;
+      }}/>,
+    this.options.selector);
+}
+
 ProtoGraph.Card.toExplain.prototype.renderEdit = function (data) {
   this.mode = 'edit';
   ReactDOM.render(
