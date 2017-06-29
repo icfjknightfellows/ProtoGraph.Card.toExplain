d5c1d2f45d90f9fb57b3acb89d1a578beec5d5b7
Prasad Bari <pb@pykih.com>
2017-06-29 15:40:12 +0530

    Add callback functions to execute code on or not on last step

--- main_edit.js
+++ main_edit.js
@@ -19,6 +19,7 @@ ProtoGraph.Card.toExplain.prototype.renderEdit = function (data) {
       schemaURL={this.options.schema_url}
       optionalConfigURL={this.options.configuration_url}
       optionalConfigSchemaURL={this.options.configuration_schema_url}
+      editData={data}
       mode={this.mode}
       ref={(e) => {
         this.containerInstance = this.containerInstance || e;
