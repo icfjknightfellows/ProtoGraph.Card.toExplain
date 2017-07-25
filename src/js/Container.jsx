ae47971d1f0120a8b6196629d1dff06659799224
Amit Badheka <ab@pykih.com>
2017-07-25 13:32:26 +0530

    Fixes condition for #

--- src/js/Container.jsx
+++ src/js/Container.jsx
@@ -64,8 +64,8 @@ export default class ExplainerCard extends React.Component {
   componentDidUpdate() {
     if (this.props.mode === 'mobile' || this.props.mode === 'laptop'){
       let elem = document.querySelector('.protograph-explainer-text')
-      this.multiLineTruncate(elem) 
-    }   
+      this.multiLineTruncate(elem)
+    }
   }
 
   multiLineTruncate(el) {
@@ -99,7 +99,7 @@ export default class ExplainerCard extends React.Component {
       return (
         <div id="protograph-div" style={styles}>
           <div className="protograph-card">
-            {(data.data.tag !== '' || data.data.tag !== undefined) ? <p className="protograph-tag">#{data.data.tag}</p>: ''}
+            {(data.data.hasOwnProperty('tag') && data.data.tag !== "undefined" && data.data.tag !== '' ) ? <p className="protograph-tag">#{data.data.tag}</p>: ''}
             <h3 className="ui header" style={{marginBottom: '15px'}}>{data.data.explainer_header}</h3>
             <p className="protograph-explainer-text">{data.data.explainer_text}</p>
           </div>
@@ -119,7 +119,7 @@ export default class ExplainerCard extends React.Component {
       return (
         <div id="protograph-div" style={styles}>
           <div className="protograph-card">
-            {(data.data.tag !== '' || data.data.tag !== undefined) ? <p className="protograph-tag">#{data.data.tag}</p>: ''}
+            {(data.data.hasOwnProperty('tag') && data.data.tag !== "undefined" && data.data.tag !== '' ) ? <p className="protograph-tag">#{data.data.tag}</p>: ''}
             <h3 className="ui header" style={{marginBottom: '15px'}}>{data.data.explainer_header}</h3>
             <p className="protograph-explainer-text">{data.data.explainer_text}</p>
           </div>
